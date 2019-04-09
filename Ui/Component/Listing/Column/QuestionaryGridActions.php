6c2cba7164daf89eb8ba6535252758f829e5e23d
ws10 <sergio@waskiri.com>
2019-04-09 10:31:01 -0400

    fix select options for new questionary

---
+++ Ui/Component/Listing/Column/QuestionaryGridActions.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ws10
+ * Date: 09-04-19
+ * Time: 10:18 AM
+ */
+
+namespace Buildateam\Quiz\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\UrlInterface;
+
+class QuestionaryGridActions extends Column
+{
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+    /**
+     * QuizIndexActions constructor.
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl('quiz/questionary/edit', ['quiz_id' => $item['entity_id']]),
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+                $item[$this->getData('name')]['delete'] = [
+                    'href' => $this->urlBuilder->getUrl('quiz/questionary/delete', ['quiz_id' => $item['entity_id']]),
+                    'label' => __('Delete'),
+                    'hidden' => false,
+                ];
+            }
+        }
+        return $dataSource;
+    }
+}
