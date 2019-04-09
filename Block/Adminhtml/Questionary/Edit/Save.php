9449400229f65a4c7f5e9c13e23ef4b598c68680
ws10 <sergio@waskiri.com>
2019-04-09 10:16:36 -0400

    fix save and save and continue button for new questionary

---
+++ Block/Adminhtml/Questionary/Edit/Save.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ws10
+ * Date: 09-04-19
+ * Time: 10:12 AM
+ */
+
+namespace Buildateam\Quiz\Block\Adminhtml\Questionary\Edit;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Buildateam\Quiz\Block\Adminhtml\GenericButton;
+
+class Save extends GenericButton implements ButtonProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Save'),
+            'class' => 'save primary',
+            'data_attribute' => [
+                'mage-init' => [
+                    'button' => ['event' => 'save']
+                ],
+            ],
+            'sort_order' => 100,
+        ];
+    }
+}
---
+++ Block/Adminhtml/Questionary/Edit/SaveAndContinueButton.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ws10
+ * Date: 09-04-19
+ * Time: 10:10 AM
+ */
+
+namespace Buildateam\Quiz\Block\Adminhtml\Questionary\Edit;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Buildateam\Quiz\Block\Adminhtml\GenericButton;
+
+class SaveAndContinueButton extends GenericButton implements ButtonProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Save and Continue Edit'),
+            'class' => 'save',
+            'data_attribute' => [
+                'mage-init' => [
+                    'button' => ['event' => 'saveAndContinueEdit'],
+                ],
+            ],
+            'sort_order' => 80,
+        ];
+    }
+}
