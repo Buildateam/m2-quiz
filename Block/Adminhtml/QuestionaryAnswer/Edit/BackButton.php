2af89ea1b91e963607fe4897391591fda83c1c17
ws10 <sergio@waskiri.com>
2019-04-10 11:35:50 -0400

    add crud answer

---
+++ Block/Adminhtml/QuestionaryAnswer/Edit/BackButton.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ws10
+ * Date: 10-04-19
+ * Time: 10:53 AM
+ */
+
+namespace Buildateam\Quiz\Block\Adminhtml\QuestionaryAnswer\Edit;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Buildateam\Quiz\Block\Adminhtml\GenericButton;
+
+class BackButton extends GenericButton implements ButtonProviderInterface
+{
+    /**
+     * @var \Buildateam\Quiz\Model\QuestionRepository
+     */
+    protected $questionRepository;
+    /**
+     * BackButton constructor.
+     * @param \Buildateam\Quiz\Model\QuestionRepository $questionRepository
+     * @param \Magento\Backend\Block\Widget\Context $context
+     * @param \Magento\Framework\Registry $registry
+     */
+    public function __construct(
+        \Buildateam\Quiz\Model\QuestionRepository $questionRepository,
+        \Magento\Backend\Block\Widget\Context $context,
+        \Magento\Framework\Registry $registry
+    ) {
+        $this->questionRepository = $questionRepository;
+        parent::__construct($context, $registry);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Back'),
+            'on_click' => sprintf("location.href = '%s';", $this->getBackUrl()),
+            'class' => 'back',
+            'sort_order' => 10
+        ];
+    }
+    /**
+     * Get URL for back (reset) button
+     *
+     * @return string
+     */
+    public function getBackUrl()
+    {
+        $question = null;
+        if ($questionId = $this->request->getParam('question_id')) {
+            try {
+                $question = $this->questionRepository->getById($questionId);
+            } catch (NoSuchEntityException $e) {
+                $question = null;
+            }
+        }
+        if ($question) {
+            return $this->getUrl('quiz/questionaryquestion/edit/', [
+                'question_id' => $question->getId(), 'quiz_id' => $question->getData('quiz_id')]);
+        } else {
+            return $this->getUrl('quiz/questionary/index/');
+        }
+    }
+
+}
---
+++ Controller/Adminhtml/QuestionaryAnswer/Save.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ws10
+ * Date: 10-04-19
+ * Time: 11:01 AM
+ */
+
+namespace Buildateam\Quiz\Controller\Adminhtml\QuestionaryAnswer;
+
+
+class Save extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\Data\Form\FormKey\Validator
+     */
+    protected $formKeyValidator;
+
+    /**
+     * @var \Buildateam\Quiz\Api\AnswerRepositoryInterface
+     */
+    protected $answerRepository;
+    /**
+     * @var \Buildateam\Quiz\Model\ImageUploader
+     */
+    protected $imageUploader;
+
+    /**
+     * @var \Buildateam\Quiz\Model\AnswerFactory
+     */
+    protected $answerFactory;
+
+    /**
+     * Save constructor.
+     * @param \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator
+     * @param \Buildateam\Quiz\Api\AnswerRepositoryInterface $answerRepository
+     * @param \Buildateam\Quiz\Model\AnswerFactory $answerFactory
+     * @param \Buildateam\Quiz\Model\ImageUploader $imageUploader
+     * @param \Magento\Backend\App\Action\Context $context
+     */
+    public function __construct(
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
+        \Buildateam\Quiz\Api\AnswerRepositoryInterface $answerRepository,
+        \Buildateam\Quiz\Model\AnswerFactory $answerFactory,
+        \Buildateam\Quiz\Model\ImageUploader $imageUploader,
+        \Magento\Backend\App\Action\Context $context
+    ) {
+        parent::__construct($context);
+        $this->formKeyValidator = $formKeyValidator;
+        $this->answerRepository = $answerRepository;
+        $this->imageUploader = $imageUploader;
+        $this->answerFactory = $answerFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if (!$this->formKeyValidator->validate($this->getRequest())) {
+            return $resultRedirect->setPath('*/*/');
+        }
+        $data = $this->getRequest()->getParams();
+        $image = null;
+        if (isset($data['image'][0]['name']) && isset($data['image'][0]['tmp_name'])) {
+            $image = $data['image'][0]['name'];
+            $this->imageUploader->moveFileFromTmp($image);
+        } elseif (isset($data['image'][0]['image']) && !isset($data['image'][0]['tmp_name'])) {
+            $image = $data['image'][0]['image'];
+        }
+        $data['image'] = $image;
+        $id = $this->getRequest()->getParam('entity_id');
+        $questionId = $data['question_id'];
+        $quizId = $this->getRequest()->getParam('quiz_id');
+        if ($id) {
+            try {
+                $answer = $this->answerRepository->getById($id);
+                $answer->setData($data);
+                $this->answerRepository->save($answer);
+                $this->messageManager->addSuccessMessage(__('Edited Answer Successfully.'));
+            } catch (\Exception $e) {
+                $quiz = null;
+                $this->messageManager->addErrorMessage(__('Error editing the Answer.'));
+            }
+        } else {
+            unset($data['entity_id']);
+            /** @var \Buildateam\Quiz\Model\Answer $answer */
+            $answer = $this->answerFactory->create();
+            try {
+                $answer->setData($data);
+                $id = $this->answerRepository->save($answer)->getId();
+                $this->messageManager->addSuccessMessage(__('Answer saved successfully.'));
+            } catch (\Exception $e) {
+                $this->messageManager->addErrorMessage(__('Error saving the Answer.'));
+            }
+        }
+        if($questionId && isset($data['back']) && $data['back'] == 'edit') {
+            return $resultRedirect->setPath('*/questionaryanswer/edit', [
+                'answer_id' => $id, 'question_id'=> $questionId, '_current' => true
+            ]);
+        } else {
+            return $resultRedirect->setPath('*/questionaryquestion/edit', [
+                'quiz_id' => $quizId, 'question_id'=> $questionId, '_current' => true
+            ]);
+        }
+
+    }
+}
